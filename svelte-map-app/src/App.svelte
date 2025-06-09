b3d279f2c52c0a5c772ed75f6931ac1ca744d372
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-06-09 16:52:58 +0100

    Add example of using L.geoJSON

--- svelte-map-app/src/App.svelte
+++ svelte-map-app/src/App.svelte
@@ -6,16 +6,27 @@
   import { onMount } from 'svelte';
   import L from 'leaflet';
 
+
+  let geojsonFeature = {
+    type:"MultiPolygon",
+    coordinates:[[[[6.16488075,50.04647446],[6.16281986,50.11296082],[6.07890368,50.11296082],[6.0768013,50.17946243],[5.74549675,50.04647446],[5.84482479,49.58156967],[6.09980154,49.4488945],[6.34645844,49.58156967],[6.33857012,49.84712601],[6.16693926,49.98000717],[6.16488075,50.04647446]]]]
+  };
+
+
   let map;
 
   onMount(() => {
-    map = L.map('map').setView([0, 0], 2);
+    map = L.map('map',
+      {crs: L.CRS.EPSG3857}
+    ).setView([0, 0], 2);
 
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      maxZoom: 5,
+      maxZoom: 10,
       minZoom: 1,
       attribution: '© ArcGIS, Powered by Esri'
     }).addTo(map);
+
+    L.geoJSON(geojsonFeature).addTo(map);
   });
 </script>
 
