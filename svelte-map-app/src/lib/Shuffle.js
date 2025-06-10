25f81edbd68a2195ee1396f08273e09d96ea9335
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-10 15:59:31 +0100

    add commented shuffle code

--- svelte-map-app/src/lib/Shuffle.js
+++ svelte-map-app/src/lib/Shuffle.js
@@ -13,3 +13,21 @@ function shuffleArray(array) {
 
 shuffleArray(years);
 export const shuffledYears = years;
+
+// function shuffleArray(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
+
+
+// function pick_year({ min_year, max_year }) {
+//   let years = [];
+//   for (let i = min_year; i <= max_year; i += 10) {
+//     years.push(i);
+//   }
+//   shuffleArray(years);
+//   export const shuffledYears = years;
+//   // return Math.floor(Math.random() * (max_year - min_year + 1)) + min_year;
+// }
