c945f1def81fb812b1b419e6cb0871e36587480e
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-06-10 10:18:15 +0100

    Add prettier

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ cd src/svelte-map-app
 <!-- npm install vite -->
 npm run dev
 ```
+
+## Linters and Formatters
+
+1. Install the pre-commit hooks by:
+   1. Installing pre-commit.
+   1. Setting up the pre-commit hooks with `pre-commit install --install-hooks`.
+   1. ESLint with Svelte plugin will now run whenever you do a `git commit` or if you run `pre-commit run`.
+1. Run prettier with `npx prettier . --write`.
+   Note that we could
--- svelte-map-app/src/App.svelte
+++ svelte-map-app/src/App.svelte
@@ -1,38 +1,48 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-
-  import { onMount } from 'svelte';
-  import L from 'leaflet';
-
-  let a;
+  import { onMount } from "svelte";
+  import L from "leaflet";
 
   let geojsonFeature = {
-    type:"MultiPolygon",
-    coordinates:[[[[6.16488075,50.04647446],[6.16281986,50.11296082],[6.07890368,50.11296082],[6.0768013,50.17946243],[5.74549675,50.04647446],[5.84482479,49.58156967],[6.09980154,49.4488945],[6.34645844,49.58156967],[6.33857012,49.84712601],[6.16693926,49.98000717],[6.16488075,50.04647446]]]]
+    type: "MultiPolygon",
+    coordinates: [
+      [
+        [
+          [6.16488075, 50.04647446],
+          [6.16281986, 50.11296082],
+          [6.07890368, 50.11296082],
+          [6.0768013, 50.17946243],
+          [5.74549675, 50.04647446],
+          [5.84482479, 49.58156967],
+          [6.09980154, 49.4488945],
+          [6.34645844, 49.58156967],
+          [6.33857012, 49.84712601],
+          [6.16693926, 49.98000717],
+          [6.16488075, 50.04647446],
+        ],
+      ],
+    ],
   };
-
 
   let map;
 
   onMount(() => {
-    map = L.map('map',
-      {crs: L.CRS.EPSG3857}
-    ).setView([0, 0], 2);
+    map = L.map("map", { crs: L.CRS.EPSG3857 }).setView([0, 0], 2);
 
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      maxZoom: 10,
-      minZoom: 1,
-      attribution: '© ArcGIS, Powered by Esri'
-    }).addTo(map);
+    L.tileLayer(
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      {
+        maxZoom: 10,
+        minZoom: 1,
+        attribution: "© ArcGIS, Powered by Esri",
+      },
+    ).addTo(map);
 
     L.geoJSON(geojsonFeature).addTo(map);
   });
 </script>
 
-
 <div id="map"></div>
+
 <!-- <main>
   <div id="map"></div>
   <div>
@@ -59,7 +69,9 @@
 </main> -->
 
 <style>
-  #map { height: 100vh; }
+  #map {
+    height: 100vh;
+  }
   .logo {
     height: 6em;
     padding: 1.5em;
