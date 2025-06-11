8eabc80ffc38a9ce68c1aa9dd3962a68703f47b8
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-11 10:04:50 +0100

    initial leaderboard page

--- my-app/src/routes/leaderboard/+page.svelte
+++ my-app/src/routes/leaderboard/+page.svelte
@@ -1,11 +1,54 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let leaderboard = [];
+  let loading = true;
+  let error = null;
+
+  onMount(async () => {
+    try {
+      const res = await fetch('http://localhost:8000/api/leaderboard/');
+      if (!res.ok) throw new Error('Failed to fetch leaderboard');
+      leaderboard = await res.json();
+    } catch (e) {
+      error = e.message;
+    } finally {
+      loading = false;
+    }
+  });
+</script>
+
 <main>
-  <h1>Clioguesser</h1>
+  <h1>Leaderboard</h1>
   
-  <p>
-    Do you think you know your history? Guess the age of this map based on the
-    polity outlines.
-  </p>
-  
+  <h2>
+    Top scorers:
+  </h2>
+
+  {#if loading}
+    <p>Loading...</p>
+  {:else if error}
+    <p style="color: red;">{error}</p>
+  {:else}
+    <table>
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each leaderboard as player, i}
+          <tr>
+            <td>{i + 1}</td>
+            <td>{player.name}</td>
+            <td>{player.score}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/if}
 
   <div id="map"></div>
   <p>
